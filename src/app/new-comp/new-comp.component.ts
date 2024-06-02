eba24681aefb32bd16ff7b5f0fe464aae0cf798a
unknown <rohinitibile25@gmail.com>
2024-06-02 12:32:38 +0530

    First Angular Hello world Project

---
+++ src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  templateUrl: './new-comp.component.html',
+  styleUrl: './new-comp.component.css'
+})
+export class NewCompComponent {
+
+}
